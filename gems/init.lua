1b5e7c601d1d62678741dc6fd33ab1441e809e60
AntumDeluge <antumdeluge@gmail.com>
2017-06-23 05:12:27 -0700

    Remove lines with 'default.generate_nyancats':
    Error: 'table index is nil'

--- gems/init.lua
+++ gems/init.lua
@@ -1410,12 +1410,12 @@ end
 if mg_params.mgname == "v6" then
 	default.register_ores()
 	default.register_mgv6_decorations()
-	minetest.register_on_generated(default.generate_nyancats)
+	--minetest.register_on_generated(default.generate_nyancats)
 elseif mg_params.mgname ~= "singlenode" then
 	default.register_biomes()
 	default.register_ores()
 	default.register_decorations()
-	minetest.register_on_generated(default.generate_nyancats)
+	--minetest.register_on_generated(default.generate_nyancats)
 end
 
 --ruby
